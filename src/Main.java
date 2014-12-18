d62005643f2feffc9d2ca93aa4ad540ef08d18b1
climerman <mkoiduste@gmail.com>
2014-12-18 04:11:59 +0200

    Milestone #2: NewPlayerGUI valmis, töötab.
    TODO:
    Põhiaken
    Mängusüsteem

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import engine.SaveSys;
+import gui.NewPlayerGUI;
+import objects.Player;
+
+/**
+ * Created by Marko on 18.12.2014.
+ */
+public class Main {
+
+    // Lõin praegu main klassi, et testida süsteemi erinevaid osasid.
+
+    public static void main(String[] args) {
+        NewPlayerGUI testGui = new NewPlayerGUI();
+
+    }
+}
--- src/gui/NewPlayerGUI.java
+++ src/gui/NewPlayerGUI.java
@@ -1,16 +1,47 @@
 package gui;
 
+import engine.NewPlayer;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by Marko on 18.12.2014.
  */
 
-public class NewPlayerGUI {
+public class NewPlayerGUI extends JFrame {
     private JTextField nameField;
     private JButton createButton;
     private JButton exitButton;
     private JTextArea infoField;
+    private JPanel rootPanel;
 
+    public NewPlayerGUI() {
+        super("New Player");
 
+        setContentPane(rootPanel);
+
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = nameField.getText();
+                NewPlayer.createPlayer(name);
+                infoField.append("| [Sys] Player created! ");
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+        setVisible(true);
+    }
 }
